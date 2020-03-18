81103321afe42e473439488bd4ba30c6ff78580b
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-03-18 19:02:10 +0200

    Ditch TinyAudioLib and finish hellotable

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# Audio programming repo
+# Audio programming
 
-This repository contains my solutions to some of the exercises in Richard Boulanger's [The Audio Programming Book](https://mitpress.mit.edu/books/audio-programming-book).
+This repository contains my solutions to some of the exercises in Boulanger
+et al.'s 
+[The Audio Programming Book](https://mitpress.mit.edu/books/audio-programming-book).
 
-## Compiling propgrams
+## Compiling and running
 
 To compile programs of this repository, change to directory containing a
 Makefile. Then run command `make`. If everything goes allright you should get
-an executable named after the directory you are in. Programs are compiled with `gcc`, though you could modify the
-Makefiles to use any C-compiler that supports C99. 
+an executable named after the directory you are in. The programs will show
+required parameters, when run without any arguments.
+
+In chapter 2 programs are compiled with `gcc`, though you could modify the
+Makefiles to use any C-compiler that supports C99.
+
+In chapter 3 programs are compiled with `g++` (C++14 and upwards).
--- chapter3/hellotable/src/main.cpp
+++ chapter3/hellotable/src/main.cpp
@@ -1,9 +1,11 @@
-#include "tinyAudioLib.h"
 #include <cmath>
 #include <iostream>
+#include <memory>
+#include <portaudio.h>
 
-constexpr int SAMPLE_RATE = 44100;
+constexpr int SAMPLE_RATE = 48000;
 constexpr size_t TABLE_LENGTH = 512;
+constexpr size_t FRAMES_PER_BUFFER = 1024;
 
 enum Waveform
 {
@@ -23,10 +25,36 @@ auto fillSine() -> void
         table[i] = (float)sin(2 * M_PI * i / TABLE_LENGTH);
 }
 
+auto fillSquare() -> void
+{
+    auto i = 0ul;
+    for (; i < TABLE_LENGTH / 2; ++i)
+        table[i] = 1;
+    for (; i < TABLE_LENGTH; ++i)
+        table[i] = -1;
+}
+
+auto fillSaw() -> void
+{
+    for (auto i = 0ul; i < TABLE_LENGTH; ++i)
+        table[i] = 1.f - (2 * (float)i / TABLE_LENGTH);
+}
+
+auto fillTriangle() -> void
+{
+    auto i = 0ul;
+    for (; i < TABLE_LENGTH / 2; ++i)
+        table[i] = 2. * i / (TABLE_LENGTH / 2.) - 1.;
+    for (; i < TABLE_LENGTH; ++i)
+        table[i] =
+            1. - (2. * (double)(i - TABLE_LENGTH / 2) / (TABLE_LENGTH / 2.));
+}
+
 auto main() -> int
 {
-    int frequency = 0;
-    int duration = 0;
+    int exitCode = 0;
+    int frequency = -1;
+    double duration = -1;
 
     std::cout << "Enter the desired frequency (Hz): ";
     std::cin >> frequency;
@@ -65,31 +93,116 @@ auto main() -> int
         fillSine();
         break;
 
+    case Waveform::Square:
+        fillSquare();
+        break;
+
+    case Waveform::Saw:
+        fillSaw();
+        break;
+
+    case Waveform::Triangle:
+        fillTriangle();
+        break;
+
     default:
         std::cerr << "Error: couldn't decide lookup table fill method\n";
         exit(EXIT_FAILURE);
         break;
     }
 
-    tinyInit();
-
-    // DEBUG:
-    std::cout << '\n' << wave << " " << frequency << " " << duration << '\n';
-
-    // Generate sound
-    const double sampleIncrement = frequency * TABLE_LENGTH / SAMPLE_RATE;
-    double phase = 0;
-    for (int i = 0; i < duration * SAMPLE_RATE; ++i)
+    int paErr = Pa_Initialize();
+    if (paErr != paNoError)
     {
-        auto sample = table[(size_t)phase];
-        outSampleMono(sample);
-        phase += sampleIncrement;
-        if (phase > TABLE_LENGTH)
-            phase -= TABLE_LENGTH;
-        std::cout << "out " << i << '\n';
+        ++exitCode;
+        std::cerr << "Error initializing portaudio: " << Pa_GetErrorText(paErr)
+                  << '\n';
+        goto cleanup;
     }
 
-    // Cleanup
-    tinyExit();
-    return 0;
+    PaStreamParameters outputParams;
+    outputParams.device = Pa_GetDefaultOutputDevice();
+    if (outputParams.device == paNoDevice)
+    {
+        ++exitCode;
+        std::cerr << "Error: no default output device\n";
+        goto cleanup;
+    }
+    outputParams.channelCount = 1;
+    outputParams.sampleFormat = paFloat32;
+    outputParams.suggestedLatency =
+        Pa_GetDeviceInfo(outputParams.device)->defaultLowOutputLatency;
+    outputParams.hostApiSpecificStreamInfo = NULL;
+
+    PaStream *stream;
+    paErr =
+        Pa_OpenStream(&stream, NULL /* no input */, &outputParams, SAMPLE_RATE,
+                      FRAMES_PER_BUFFER, paClipOff, NULL, NULL);
+    if (paErr != paNoError)
+    {
+        std::cerr << "Error: failed to open output stream.\n"
+                  << Pa_GetErrorText(paErr) << '\n';
+        ++exitCode;
+        goto cleanup;
+    }
+
+    paErr = Pa_StartStream(stream);
+    if (paErr != paNoError)
+    {
+        std::cerr << "Error: failed to start stream\n"
+                  << Pa_GetErrorText(paErr) << '\n';
+        ++exitCode;
+        goto cleanup;
+    }
+
+    {
+        const size_t bufferCount = (duration * SAMPLE_RATE) / FRAMES_PER_BUFFER;
+        // std::vector<float> buffer(FRAMES_PER_BUFFER);
+        std::unique_ptr<float[]> buffer(new float[FRAMES_PER_BUFFER]);
+        const double sampleIncrement = frequency * TABLE_LENGTH / SAMPLE_RATE;
+        // How many buffers have to be filled and output
+        double phase = 0; // Phase of waveform
+        for (auto i = 0lu; i < bufferCount; ++i)
+        {
+            // Fill buffer with wave
+            for (auto j = 0lu; j < FRAMES_PER_BUFFER; ++j)
+            {
+                buffer[j] = table[(size_t)phase];
+                phase += sampleIncrement;
+                if (phase > TABLE_LENGTH) // return to beginning of table
+                    phase -= TABLE_LENGTH;
+            }
+            // Output buffer to stream
+            paErr = Pa_WriteStream(stream, buffer.get(), FRAMES_PER_BUFFER);
+            if (paErr != paNoError)
+            {
+                std::cerr << "Error: writing to stream failed\n"
+                          << Pa_GetErrorText(paErr) << '\n';
+                ++exitCode;
+                goto cleanup;
+            }
+        }
+    }
+
+    paErr = Pa_StopStream(stream);
+    if (paErr != paNoError)
+    {
+        std::cerr << "Error: failed to stop stream\n";
+        ++exitCode;
+        goto cleanup;
+    }
+
+    paErr = Pa_CloseStream(stream);
+    if (paErr != paNoError)
+    {
+        std::cerr << "Error: failed to close stream\n";
+        ++exitCode;
+        goto cleanup;
+    }
+
+cleanup:
+    if (paErr == paNoError)
+        if (Pa_Terminate() != paNoError)
+            std::cerr << "Error: failed to terminate portaudio\n";
+    return exitCode;
 }
