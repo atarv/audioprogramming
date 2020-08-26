b21ccb03261a6fa26c1089acdd8d21a3dca0810b
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-08-26 19:31:11 +0300

    Add command line arguments to filter experiment

--- chapter6/filter/src/main.rs
+++ chapter6/filter/src/main.rs
@@ -1,4 +1,6 @@
-/// Small application to test applying various types of filters to a saw wave
+//! Small application to test applying various types of filters to a saw wave
+
+use clap::{App, Arg};
 use hound;
 use std::f32::consts::PI;
 
@@ -104,13 +106,62 @@ fn highpass<'a>(
 
 fn main() {
     // TODO: Get command line arguments
+    let matches = App::new("Filter experiment")
+        .author("Aleksi Tarvainen")
+        .about("Small program to experiment with filters")
+        .arg(
+            Arg::with_name("OUTPUT")
+                .takes_value(true)
+                .required(true)
+                .help("Path of output file"),
+        )
+        .arg(
+            Arg::with_name("type")
+                .short("f")
+                .long("filter")
+                .takes_value(true)
+                .required(true)
+                .possible_values(&["lpf", "hpf", "none"])
+                .help("Set filter type"),
+        )
+        .arg(
+            Arg::with_name("frequency")
+                .takes_value(true)
+                .required(true)
+                .short("z")
+                .long("frequency")
+                .help("Oscillator frequency in Hz"),
+        )
+        .arg(
+            Arg::with_name("cutoff")
+                .takes_value(true)
+                .required(true)
+                .short("c")
+                .long("cutoff")
+                .help("Cutoff frequency of filter"),
+        )
+        .arg(
+            Arg::with_name("duration")
+                .takes_value(true)
+                .required(true)
+                .short("d")
+                .long("duration")
+                .help("Duration of tone in seconds"),
+        )
+        .get_matches();
+
     let mut wavetable = [0i16; WAVETABLE_LENGTH];
     fill_saw_table(&mut wavetable);
 
-    let mut osc = Oscillator::create(200.0, &wavetable);
+    let freq = matches
+        .value_of("frequency")
+        .unwrap()
+        .parse::<f32>()
+        .unwrap();
+    let mut osc = Oscillator::create(freq, &wavetable);
 
     let mut writer = hound::WavWriter::create(
-        "out.wav",
+        matches.value_of("OUTPUT").unwrap(),
         hound::WavSpec {
             bits_per_sample: 16,
             sample_rate: SAMPLE_RATE,
@@ -120,13 +171,28 @@ fn main() {
     )
     .unwrap();
 
-    let time = 5.0f32;
+    let duration = matches
+        .value_of("duration")
+        .unwrap()
+        .parse::<f32>()
+        .unwrap();
     let mut delay = 0;
-    for _ in 0..((time * SAMPLE_RATE as f32) as usize) {
-        let s = osc.tick(200.0);
+    let cutoff_freq = matches.value_of("cutoff").unwrap().parse::<f32>().unwrap();
+    for _ in 0..((duration * SAMPLE_RATE as f32) as usize) {
+        let s = osc.tick(freq);
         let mut buf = [s];
-        // lowpass(&mut buf, 1000., &mut delay, SAMPLE_RATE);
-        highpass(&mut buf, 1000., &mut delay, SAMPLE_RATE);
+        match matches.value_of("type").unwrap() {
+            "lpf" => {
+                lowpass(&mut buf, cutoff_freq, &mut delay, SAMPLE_RATE);
+                ()
+            }
+            "hpf" => {
+                highpass(&mut buf, cutoff_freq, &mut delay, SAMPLE_RATE);
+                ()
+            }
+            "none" => (),
+            _ => panic!("No filter type provided. This shouldn't happen."),
+        }
         writer.write_sample(buf[0]).unwrap();
     }
     writer
